b02073a353ec0ea2da58f2c36b7a8e8e0f0c2933
漠北-暗语寒飞 <1223050251@qq.com>
2021-03-09 18:08:06 +0800

    0309修改

--- public/python/get_data.py
+++ public/python/get_data.py
@@ -64,7 +64,7 @@ if fil_usdt_select_sql_res:
     fil_usdt_update_res = cursor.execute(fil_usdt_update_sql)
     if fil_usdt_update_res == 1:
         conn.commit()
-    fil_update_sql = "update sys_setting set value='%s' where sign='USDT2FIL'" % (fil_usdt[1])
+    fil_update_sql = "update sys_setting set value='%s' where sign='USDT2FIL'" % (1 / fil_usdt[1])
     fil_update_res = cursor.execute(fil_update_sql)
     if fil_update_res == 1:
         conn.commit()
