a1dfded8b3ce3a00e2fca78f1dc1b1ef53882b1b
漠北-暗语寒飞 <1223050251@qq.com>
2021-03-09 14:14:39 +0800

    充值开放

--- app/index/controller/Mefund.php
+++ app/index/controller/Mefund.php
@@ -44,7 +44,6 @@ class Mefund extends Index{
     }
 
     public function 充值(){
-        return redirect('/');
         if($this->user->address == ''){
             $kuake_ip = Env::get('ANER_ADMIN.KUAKE_IP');
             $url = "http://". $kuake_ip ."/tron/createAddress?userId=" . $this->user_id;
